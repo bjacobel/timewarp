1dd62f51c02b12bcb3a0f4b2adc4a443047df6c3
Brian Jacobel <bjacobel@gmail.com>
2014-12-23 12:43:38 -0600

    Q-ifiying

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,14 @@
 Zepto(function($){
     var today = moment().format("M-D");
+    getReelForDate(today).then(function(data){
+        $(".reel").attr("src", data.url);
+    }).catch(function(err){
+        console.error(err);
+    });
+});
+
+var getReelForDate = function(today){
+    var deferred = Q.defer();
     $.ajax({
         type: "GET",
         url: "http://archive.org/advancedsearch.php",
@@ -14,12 +23,17 @@ Zepto(function($){
         dataType: 'jsonp',
         success: function(data){
             all_reels = data.response.docs;
-            reel = all_reels[Math.floor(Math.random()*all_reels.length)];
-            reel.url = "http://archive.org/download/"+reel.identifier+"/"+reel.identifier+"_512kb.mp4";
-            $(".reel").attr("src", reel.url);
+            if(all_reels.length > 0){
+                reel = all_reels[Math.floor(Math.random()*all_reels.length)];
+                reel.url = "http://archive.org/download/"+reel.identifier+"/"+reel.identifier+"_512kb.mp4";
+                deferred.resolve(reel);
+            } else {
+                deferred.reject("No newsreels found for "+today);
+            }
         },
         error: function(xhr, type){
-            console.error(type+" error! "+xhr.statusText);
+            deferred.reject(type+" error! "+xhr.statusText);
         }
     });
-});
+    return deferred.promise;
+};
