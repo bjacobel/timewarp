a06b776d8e8ba20e3cc1dd7c19d5210ddaf2ce70
Brian Jacobel <bjacobel@gmail.com>
2014-12-23 16:35:09 -0600

    Dropdown box

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,32 +9,32 @@ Zepto(function($){
 
 var getReelForDate = function(today){
     var deferred = Q.defer();
-    $.ajax({
-        type: "GET",
-        url: "http://archive.org/advancedsearch.php",
-        data: {
-            "q": "collection:(universal_newsreels) AND identifier:(*"+today+"*)",
-            "fl[]": "description, identifier, title",
-            "sort": "avg_rating+desc",
-            "rows": "50",
-            "page": "1",
-            "output": "json",
-        },
-        dataType: 'jsonp',
-        success: function(data){
-            all_reels = data.response.docs;
-            if(all_reels.length > 0){
-                reel = all_reels[Math.floor(Math.random()*all_reels.length)];
-                reel.url = "http://archive.org/download/"+reel.identifier+"/"+reel.identifier+"_512kb.mp4";
-                deferred.resolve(reel);
-            } else {
-                deferred.reject("No newsreels found for "+today);
-            }
-        },
-        error: function(xhr, type){
-            deferred.reject(type+" error! "+xhr.statusText);
-        }
-    });
+    // $.ajax({
+    //     type: "GET",
+    //     url: "http://archive.org/advancedsearch.php",
+    //     data: {
+    //         "q": "collection:(universal_newsreels) AND identifier:(*"+today+"*)",
+    //         "fl[]": "description, identifier, title",
+    //         "sort": "avg_rating+desc",
+    //         "rows": "50",
+    //         "page": "1",
+    //         "output": "json",
+    //     },
+    //     dataType: 'jsonp',
+    //     success: function(data){
+    //         all_reels = data.response.docs;
+    //         if(all_reels.length > 0){
+    //             reel = all_reels[Math.floor(Math.random()*all_reels.length)];
+    //             reel.url = "http://archive.org/download/"+reel.identifier+"/"+reel.identifier+"_512kb.mp4";
+    //             deferred.resolve(reel);
+    //         } else {
+    //             deferred.reject("No newsreels found for "+today);
+    //         }
+    //     },
+    //     error: function(xhr, type){
+    //         deferred.reject(type+" error! "+xhr.statusText);
+    //     }
+    // });
     return deferred.promise;
 };
 
@@ -57,10 +57,16 @@ Zepto(function($){
 Zepto(function($){
     window.setInterval(function(){
         if($(".dots-animate").text()=="..."){
-            $(".dots-animate").text("");
+            $(".dots-animate").text(".");
         } else {
             $(".dots-animate").text($(".dots-animate").text()+".")
         }
     },500);
 
+});
+
+Zepto(function($){
+    $(".datepicker").click(function(){
+        $(".picker-dropdown").toggleClass("hide");
+    });
 });
