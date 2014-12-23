2a08649f71ab8852f123fbb7100623262bcc3a03
Brian Jacobel <bjacobel@gmail.com>
2014-12-23 17:47:07 -0600

    Passing jshint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,3 @@
-// Get the video on page load
-Zepto(function($){
-    var today = moment().format("MM-DD");
-    updatePage(today);
-});
-
 // Talk to archive.org
 var getReelForDate = function(today){
     var deferred = Q.defer();
@@ -18,11 +12,11 @@ var getReelForDate = function(today){
             "page": "1",
             "output": "json",
         },
-        dataType: 'jsonp',
+        dataType: "jsonp",
         success: function(data){
-            all_reels = data.response.docs;
+            var all_reels = data.response.docs;
             if(all_reels.length > 0){
-                reel = all_reels[Math.floor(Math.random()*all_reels.length)];
+                var reel = all_reels[Math.floor(Math.random()*all_reels.length)];
                 reel.url = "http://archive.org/download/"+reel.identifier+"/"+reel.identifier+"_512kb.mp4";
                 deferred.resolve(reel);
             } else {
@@ -38,8 +32,8 @@ var getReelForDate = function(today){
 
 // Pipe in video data to the page
 var updatePage = function(chosen_date){
-    if(chosen_date != moment().format("MM-DD")){
-        $(".datepicker").html(chosen_date+' <i class="fa fa-chevron-down"></i>');
+    if(chosen_date !== moment().format("MM-DD")){
+        $(".datepicker").html(chosen_date+" <i class='fa fa-chevron-down'></i>");
     }
 
     getReelForDate(chosen_date).then(function(data){
@@ -48,11 +42,17 @@ var updatePage = function(chosen_date){
         $(".reel-description").text(data.description);
     }).catch(function(err){
         console.error(err);
-        if(err=="No newsreels found"){
+        if(err === "No newsreels found"){
             $(".reel-title").text("Nothing found on this date. Warp again!");
         }
     });
 };
+
+// Get the video on page load
+Zepto(function(){
+    var today = moment().format("MM-DD");
+    updatePage(today);
+});
 
 // Fix the height of the video container
 Zepto(function($){
@@ -67,10 +67,10 @@ Zepto(function($){
 // Animate loading dots while video is being fetched
 Zepto(function($){
     window.setInterval(function(){
-        if($(".dots-animate").text()=="..."){
+        if($(".dots-animate").text() === "..."){
             $(".dots-animate").text(".");
         } else {
-            $(".dots-animate").text($(".dots-animate").text()+".")
+            $(".dots-animate").text($(".dots-animate").text()+".");
         }
     },500);
 
@@ -96,9 +96,9 @@ Zepto(function($){
         var month = $(".datepicker-input#month").val();
 
         // validate the form
-        if (moment(new Date(2000, month, day))._d != "Invalid Date"){
+        if (moment(new Date(2000, month, day))._d !== "Invalid Date"){
             $(".datepicker-dropdown").toggleClass("hide");
-            $(".reel-title").html('Warping through time<span class="dots-animate"></span>');
+            $(".reel-title").html("Warping through time<span class='dots-animate'></span>");
             updatePage(moment(new Date(2000, month-1, day)).format("MM-DD"));
         } else {
             alert("Not a valid date.");
